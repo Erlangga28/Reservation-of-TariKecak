b4d1bfcf9c9d24d4206f25ac1ed970a2c835412a
Azkaaysar <pribadiazka4@gmail.com>
2022-12-05 23:14:52 +0700

    Create Design Ticket

--- Ticket.php
+++ Ticket.php
@@ -1,94 +1,99 @@
-<?php
-require('connection.php');
-?>
-<style>
-<?php include 'Styling.css'; ?>
-</style>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reservation Of Tari Kecak</title>
-    <link rel = "stylesheet" type ="text/css" href="Styling.css">
-</head>
-    <body>
-        <header>
-            <div class="Menu" style="background-color: whitesmoke;">
-                <ul>
-                    <img src="Tarikecak-logo.png">
-                </ul>
-            </div>
-            <div id="mySidepanel" class="sidepanel">
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-                <a href="HomePage.html"> Home </a>
-                <a href="Shop.html">Custome</a>
-                <a href="Ticket.php"> Tickets </a>
-                <a href="#">About</a>
-            </div>
-          
-            <button class="openbtn" onclick="openNav()">☰</button>
-            <script>
-                function openNav() {
-                    document.getElementById("mySidepanel").style.width = "300px";
-                }
-          
-                function closeNav() {
-                    document.getElementById("mySidepanel").style.width = "0";
-                }
-            </script>
-        </header>
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Reservation Of Tari Kecak</title>
+                    <link rel = "stylesheet" type ="text/css" href="Styling.css">
+                </head>
+                <header>
+                    <div class="Menu">
+                        <ul>
+                            <li><a href ="HomePage.html"> Home </a></li>
+                            <li><a href = "Shop.html"> Shop </a></li>
+                        </ul>
+                    </div>
+                    <div id="mySidepanel" class="sidepanel">
+                        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+                        <a href="HomePage.html"> Home </a>
+                        <a href="Shop.html">Custome</a>
+                        <a href="Ticket.html"> Tickets </a>
+                        <a href="#">About</a>
+                    </div>
+                  
+                    <button class="openbtn" onclick="openNav()">☰</button>
+                    <script>
+                        function openNav() {
+                            document.getElementById("mySidepanel").style.width = "300px";
+                        }
+                  
+                        function closeNav() {
+                            document.getElementById("mySidepanel").style.width = "0";
+                        }
+                    </script>
+                </header>
+                <body>
+                    <div class="About">
+                        <h2> Ticket Price </h2>
+                        <!-- <p> In the 1930s, Balinese artist Wayan Limbak and German painter Walter Spies created the Kecak dance. This dance is inspired by the traditional rituals performed by the Balinese people which were later adapted in the Ramayana story in Hindu belief to be displayed as an art show when tourists come to Bali.
+                            The Kecak dance is usually performed by tens of shirtless men wearing plaid cloth from the waist to above the knees.
+                            The Kecak dance was first performed in several villages, one of which was Bona Village, Gianyar. However, it has grown to all regions in Bali and is always presented during activities such as festivals held by the government and the private sector.
+                            Read the detikTravel article, "Kecak Dance: History,<span id="dots">...</span><span id="more"> Meaning and Place of Performance"In the 1930s, Balinese artist Wayan Limbak and German painter Walter Spies created the Kecak dance. This dance is inspired by the traditional rituals performed by the Balinese people which were later adapted in the Ramayana story in Hindu belief to be displayed as an art show when tourists come to Bali.
+                            The Kecak dance is usually performed by tens of shirtless men wearing plaid cloth from the waist to above the knees.
+                            The Kecak dance was first performed in several villages, one of which was Bona Village, Gianyar. However, it has grown to all regions in Bali and is always presented during activities such as festivals held by the government and the private sector.
+                            Read the detikTravel article, "Kecak Dance: History, Meaning and Place of Performance"
+                        </span></p> -->
+                        <body>
+                <body>
+                    <div class="background">
+                        <div class="shape"></div>
+                        <div class="shape"></div>
+                    </div>
+                    <form>
+                        <h3>Reservation Ticket</h3>
+                
+                        <label for="username">Your Name </label>
+                        <input type="text" placeholder="Fullname" id="username">
 
+                        <label for="password">Your E-mail</label>
+                        <input type="email" placeholder="e-mail@gmail.com" id="password">
+      
+                        <label for="password">Phone Number</label>
+                        <input type="number" placeholder="+62" id="password">
+      
+                        <label for="password">Date of Reservation</label>
+                        <input type="datetime-local" placeholder="e-mail@gmail.com" id="password">
+      
+                        <label for="password">Number of Ticket</label>
+                        <input type="number" placeholder="Pcs" id="password">
 
-    <form action="Ticket.php" method="post">
-        <div class="elem-group">
-          <label for="name">Your Name</label>
-          <input type="text" id="name" name="visitor_name" placeholder="Name" required>
-        </div>
-        <div class="elem-group">
-          <label for="email">Your E-mail</label>
-          <input type="email" id="email" name="Email" placeholder="Erlangga.Wahyu@email.com" required>
-        </div>
-        <div class="elem-group">
-          <label for="phone">Your Phone</label>
-          <input type="tel" id="phone" name="Phone" placeholder="08*****" required>
-        </div>
-        <div class="elem-group inlined">
-          <label for="ticket">Tickets</label>
-          <input type="number" id="AmountofTickets" name="total_ticket" placeholder="2" min="1" required>
-        </div>
-        <div class="elem-group inlined">
-          <label for="date">Date</label>
-          <input type="date" id="date" name="Date" required>
-        </div>
-        <div class="elem-group">
-        <button type="submit" name="save" value="Buy">Book The Tickets</button>
-              </div>
-      </form>
-    </body>
-    <?php
-    if(isset($_POST['save']))
-    { 
-         $Name = $_POST['name'];
-         $Email = $_POST['email'];
-         $Phone = $_POST['phone'];
-         $Ticket = $_POST['ticket'];
-         $Date = $_POST['date'];
-
-         $sql_query="insert into tiket (Name,Email,Phone,Ticket,Date) values ('$Name','$Email','$Phone','$Ticket','$Date')";
-    
-         if (mysqli_query($conn, $sql_query)) 
-         {
-            echo "New Details Entry inserted successfully !";
-         } 
-         else
-         {
-            echo "Error: " . $sql . "" . mysqli_error($conn);
-         }
-         mysqli_close($conn);
-
-        }
-        ?>
-</html>
+                        <div class="social">
+                          <div class="login"><i class="fab fa-login"></i>  Book Ticket</div>
+                        </div>
+                    </form>
+                </body>
+                <!-- <body>
+                    <div class="About">
+                        <h1> Ticket Price </h1>
+                        <!-- <p> In the 1930s, Balinese artist Wayan Limbak and German painter Walter Spies created the Kecak dance. This dance is inspired by the traditional rituals performed by the Balinese people which were later adapted in the Ramayana story in Hindu belief to be displayed as an art show when tourists come to Bali.
+                            The Kecak dance is usually performed by tens of shirtless men wearing plaid cloth from the waist to above the knees.
+                            The Kecak dance was first performed in several villages, one of which was Bona Village, Gianyar. However, it has grown to all regions in Bali and is always presented during activities such as festivals held by the government and the private sector.
+                            Read the detikTravel article, "Kecak Dance: History,<span id="dots">...</span><span id="more"> Meaning and Place of Performance"In the 1930s, Balinese artist Wayan Limbak and German painter Walter Spies created the Kecak dance. This dance is inspired by the traditional rituals performed by the Balinese people which were later adapted in the Ramayana story in Hindu belief to be displayed as an art show when tourists come to Bali.
+                            The Kecak dance is usually performed by tens of shirtless men wearing plaid cloth from the waist to above the knees.
+                            The Kecak dance was first performed in several villages, one of which was Bona Village, Gianyar. However, it has grown to all regions in Bali and is always presented during activities such as festivals held by the government and the private sector.
+                            Read the detikTravel article, "Kecak Dance: History, Meaning and Place of Performance"
+                        </span></p> -->
+                        <body> -->
+                            <table border="1">
+                                <tr>
+                                    <td>Student :</td>
+                                    <td>Non-Student :</td>
+                                </tr>
+                                <tr>
+                                    <td>Rp65.000</td>
+                                    <td>Rp130.000</td>
+                                </tr>
+                            </table>
+                        </body>
+            </html>
