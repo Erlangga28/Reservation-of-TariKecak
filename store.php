a6d2d369875e043d6f55ecd910b40abc24ed4ffc
Erlangga28 <Erlanggawahyu21@gmail.com>
2022-12-11 19:02:23 +0700

    change

--- store.php
+++ store.php
@@ -13,16 +13,16 @@
                     <h1 class="uppercase pl-5 py-4 text-lg font-sans font-bold">Kecak</h1>
                     <ul class="hidden lg:flex items-center text-[18px] font-semibold pl-32">
                         <li class="hover:underline  underline-offset-4 decoration-2 decoration-white py-2 rounded-lg px-5">
-                            <a href="HomePage.php">Home</a>
+                            <a href="test.php">Home</a>
                         </li>
                         <li class="hover:underline underline-offset-4 decoration-2 decoration-white py-2 rounded-lg px-5">
-                            <a href="Ticket.php">Ticket</a>
+                            <a href="buyticket.php">Ticket</a>
                         </li>
                         <li class="hover:underline underline-offset-4 decoration-2 decoration-white py-2 rounded-lg px-5">
                             <a href="#">About</a>
                         </li>
                         <li class="hover:underline underline-offset-4 decoration-2 decoration-white py-2 rounded-lg px-5">
-                            <a href="Shop.php">Store</a></li>
+                            <a href="store.php">Store</a></li>
                         </ul>
                 <div class="text-white text-center text-base pr-5  inline-flex">
                     <a href="#" class="w-8 h-8 inline-block rounded-full pt-[6px] hover:text-blue-500">
