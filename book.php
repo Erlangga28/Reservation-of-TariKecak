502d4c63359ba1756aa2761b1bb6a39c194c90c5
Erlangga28 <Erlanggawahyu21@gmail.com>
2022-12-19 08:07:14 +0700

    add table

--- book.php
+++ book.php
@@ -12,6 +12,16 @@ include('data/connection.php');
 </head>
 
 <body>
+<table class="table mt-5 table-bordered">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email </th>
+                                <th scope="col">Ticket</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
 <?php
 			$sql = "SELECT * FROM buyticket";
 			$result = $conn->query($sql);
@@ -19,13 +29,32 @@ include('data/connection.php');
 			if ($result->num_rows > 0) {
 			  // output data of each row
 			  while($row = $result->fetch_assoc()) {
-				echo "name: " . $row["custname"]. " - email: " . $row["custemail"]. " ticket:" . $row["ticketselect"]. "<br>";
+				$name= $row['custname'];
+				$email= $row['custemail'];
+				$ticket= $row['ticketselect'];
 			  }
 			} else {
 			  echo "0 results";
 			}
 			$conn->close();
 			?>
+			<tr>
+                                    <td><?php echo strtoupper($name) ?></td>
+                                    <td><?php echo $email ?></td>
+                                    <td><b><?php echo $ticket ?></b></td>
+                                    <td>
+                                        <center>
+                                            <?php
+                                            if ($ticket > 0) {
+                                                echo "<a href=\"accept.php?id1=" . $email . "&id2=" . $name . "\" class=\"btn btn-success\">Accept</a>";
+                                            }
+                                            ?>
+                                            <a href="reject.php?id1=<?php echo $email ?>&id2=<?php echo $name ?>" class="btn btn-danger">Reject</a>
+                                        </center>
+                                    </td>
+                                </tr>
+			</tbody>
+		</table>
     
 </body>
 </html>
